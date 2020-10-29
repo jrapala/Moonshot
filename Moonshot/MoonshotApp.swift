03dc97dc04ba84438bb8078ed336bc3f49af794f
Juliette Rapala <jrapala@gmail.com>
2020-10-28 20:54:59 -0500

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Juliette Rapala on 10/28/20.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
