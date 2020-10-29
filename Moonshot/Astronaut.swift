32b03f165f3eb20fbee64eb78ea44ee3806cb461
Juliette Rapala <jrapala@gmail.com>
2020-10-29 18:45:48 -0500

    feat: extend Bundle and read file

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,12 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Juliette Rapala on 10/29/20.
+//
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
---
+++ Moonshot/Bundle-Decodable.swift
@@ -0,0 +1,29 @@
+//
+//  Bundle-Decodable.swift
+//  Moonshot
+//
+//  Created by Juliette Rapala on 10/29/20.
+//
+
+import Foundation
+
+// extend bundle to give it some additional functionality
+extension Bundle {
+    func decode(_ file: String) -> [Astronaut] {
+        guard let url = self.url(forResource: file, withExtension: nil) else {
+            fatalError("Failed to locate \(file) in bundle.")
+        }
+        
+        guard let data = try? Data(contentsOf: url) else {
+            fatalError("Failed to load \(file) from bundle.")
+        }
+        
+        let decoder = JSONDecoder()
+        
+        guard let loaded = try? decoder.decode([Astronaut].self, from: data) else {
+            fatalError("Failed to decode \(file) from bundle.")
+        }
+        
+        return loaded
+    }
+}
